9eb8a15ad85e9b88806c4b7319f9c6e114f92969
Andy Fone <andy.fone@xero.com>
2018-12-19 09:53:49 +1300

    hack puzzle into rachfaces page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,34 @@ class GamesController < ApplicationController
     @user = current_user
     @puzzle = Puzzle.find(params[:puzzle_id])
     @start_time = Time.now
+
+    # Andys hax
+    colors = { '0': "#ffffff", '1': "#000000", '2': "#BADA55" }
+    vertical_clues = [
+      [{ length: 1, color: 2 }, { length: 4, color: 1 }],
+      [{ length: 1, color: 2 }, { length: 3, color: 1 }],
+      [{ length: 1, color: 2 }, { length: 2, color: 1 }],
+      [{ length: 1, color: 2 }, { length: 1, color: 1 }],
+      []
+    ]
+    horizontal_clues = [
+      [{ length: 1, color: 2 }],
+      [{ length: 1, color: 1 }, { length: 1, color: 2 }],
+      [{ length: 2, color: 1 }, { length: 1, color: 2 }],
+      [{ length: 3, color: 1 }, { length: 1, color: 2 }],
+      [{length: 4, color: 1}]
+    ]
+
+    @game_props = {
+      name: 'This is a test game!',
+      width: 5,
+      height: 5,
+      vertical_clues: vertical_clues,
+      horizontal_clues: horizontal_clues,
+      colors: colors,
+      debug: '2000012000112001112011110'
+    }
+
   end
 
   def finish
